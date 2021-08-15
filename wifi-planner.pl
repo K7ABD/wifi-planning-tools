bc3aa5e36d5650404a6b5514f4f26d6d67b6208e
Andrew - K7ABD <50472987+K7ABD@users.noreply.github.com>
2021-08-15 13:20:40 -0700

    initial checkin

---
+++ wifi-planner.pl
@@ -0,0 +1,192 @@
+#!/usr/bin/perl
+
+use strict;
+
+my $DEBUG      = 0;
+my $ITERATIONS = 10000;
+
+my @aps = ('Basement', 'Kitchen', 'Living Room', 'Master', 'Office', 'Guest Room');
+my @channels = ('1', '6', '11');
+my @attenuations = (0, -4, -8, -12, -16, -200); # H, H/M, M, M/L, L, Off
+
+my %locations; # per access point
+$locations{'Living Room'} = [-66, -79, -41, -70, -54, -91];
+$locations{'Dining Room'} = [-75, -58, -60, -63, -55, -68];
+$locations{'Office'}      = [-80, -62, -70, -60, -44, -85];
+$locations{'Sunroom'}     = [-91, -61, -89, -72, -78, -84];
+$locations{'Kitchen'}     = [-83, -47, -72, -55, -53, -73];
+
+
+my %noises; # (1, 6, 11);
+$noises{'Living Room'} = [-93, -94, -90];
+$noises{'Dining Room'} = [-90, -96, -90];
+$noises{'Office'}      = [-94, -94, -84];
+$noises{'Sunroom'}     = [-92, -87, -89];
+$noises{'Kitchen'}     = [-93, -85, -82];
+
+my %channel_indexes;
+for (my $i = 0; $i < scalar(@channels); $i++)
+{
+    $channel_indexes{$channels[$i]} = $i;
+}
+
+
+my $best_worst_snr = 0;
+my $best_plan = '';
+my $best_report = '';
+for (my $i = 0; $i < $ITERATIONS; $i++)
+{
+    my %ap_chans;
+    my @loc_snr;
+    my $report = '';
+
+    #TODO: ap_chans needs to be updated to ch,atten format including generating it and reading it
+    #      then anywhere we read from $locations needs to handle power levels
+
+    # randomly assign channels to APs:
+    foreach my $ap (@aps)
+    {
+        $ap_chans{$ap} = set_channel_and_attenuation($channels[int(rand(scalar(@channels)))],
+                                               $attenuations[int(rand(scalar(@attenuations)))]);
+    }
+
+    # foreach location, find the strongest AP:
+    foreach my $loc (keys %locations)
+    {
+        debug("Looking at Location '$loc':\n");
+
+
+        # find the strongest AP:
+        my $best_rssi = -200;
+        my $best_ap_index = -1;
+        debug("    Looking for strongest AP...\n");
+        for (my $ap_index = 0; $ap_index < scalar(@aps); $ap_index++)
+        {
+            my $loc_ap_rssi = get_rssi($ap_chans{$aps[$ap_index]}, $locations{$loc}[$ap_index]);
+
+            debug("        AP '" . $aps[$ap_index] . "' has RSSI of $loc_ap_rssi\n");
+            if ($loc_ap_rssi > $best_rssi)
+            {
+                $best_rssi = $loc_ap_rssi;
+                $best_ap_index = $ap_index;
+            }
+        }
+
+        debug("        ** Best AP is '" . $aps[$best_ap_index] . "' at $best_rssi\n");
+        
+        my $best_ap_channel      = get_channel($ap_chans{$aps[$best_ap_index]});
+        my $highest_noise        = $noises{$loc}[$channel_indexes{$best_ap_channel}];
+        my $highest_noise_source = 'Neighbors';
+
+        # find the noise level (either from external noise, or from other APs)
+        for (my $ap_index = 0; $ap_index < scalar(@aps); $ap_index++)
+        {
+            my $ch = get_channel($ap_chans{$aps[$ap_index]});
+            if ($ch eq $best_ap_channel && $ap_index != $best_ap_index)
+            {
+                my $rssi = get_rssi($ap_chans{$aps[$ap_index]}, $locations{$loc}[$ap_index]);
+                if ($rssi > $highest_noise)
+                {
+                    $highest_noise = $rssi;
+                    $highest_noise_source = "Local-AP (" . $aps[$ap_index] . ")";
+                }
+            }
+        }
+
+        my $snr = $best_rssi - $highest_noise;
+
+        push (@loc_snr, $snr);
+
+        my $rep = sprintf("%-20s %-20s %10s %8s %17s %-32s\n", $loc, $aps[$best_ap_index], $best_rssi, $snr, $highest_noise, $highest_noise_source);
+
+        debug($rep);
+        $report .= $rep;
+    }
+
+    my $worst_snr = $loc_snr[0];
+    my $sum = 0;
+    foreach my $snr (@loc_snr)
+    {
+        $sum += $snr;
+
+        if ($snr < $worst_snr)
+        {
+            $worst_snr = $snr;
+        }
+    }
+
+    my $avg_snr = $sum / scalar(@loc_snr);
+
+    my $plan = '';
+    foreach my $ap_ch (keys %ap_chans)
+    {
+        my $ch = get_channel($ap_chans{$ap_ch});
+        my $at = get_atten  ($ap_chans{$ap_ch});
+        $plan .= sprintf("%-20s %8s %12d\n", $ap_ch, $ch, $at);
+    }
+
+    if ($worst_snr > $best_worst_snr)
+    {
+        $best_worst_snr = $worst_snr;
+        $best_plan      = $plan;
+        $best_report    = $report;
+    }
+}
+
+print "\n****** Access Point and Channel Configuration ****** \n\n";
+printf ("%-20s %8s %12s\n", "Access Point", "Channel", "Attenuation");
+printf ("%-20s %8s %12s\n", "------------", "-------", "-----------");
+print $best_plan;
+
+print "\n\n****** Location - predicted best AP and SnR\n\n";
+#At Location 'Living Room', best AP is 'Living Room' on Channel 11 with RSSI -41 and SnR of 33 (highest noise is -74 from Local-AP (Master))
+printf("%-20s %-20s %10s %8s %17s %-32s\n", 'Location', 'Best AP', 'RSSI (dBm)', 'SnR (dB)', 'Noise Level (dBm)', 'Noise Source');
+printf("%-20s %-20s %10s %8s %17s %-32s\n", '--------', '-------', '----------', '--------', '-----------------', '------------');
+print $best_report;
+
+print "\nWorst SNR: $best_worst_snr\n";
+
+
+sub get_channel
+{
+    my ($ch_atten) = @_;
+
+    my ($ch,$atten) = split('\t', $ch_atten);
+
+    return $ch;
+}
+
+sub get_atten
+{
+    my ($ch_atten) = @_;
+  
+    my ($ch, $atten) = split('\t', $ch_atten);
+
+    return $atten;
+}
+
+sub get_rssi
+{
+    my ($ch_atten, $high_rssi) = @_;
+
+    my ($ch,$atten) = split('\t', $ch_atten);
+
+    return $high_rssi + $atten;
+}
+
+sub set_channel_and_attenuation
+{
+    my ($channel, $attenuation) = @_;
+
+    return "$channel\t$attenuation";
+}
+
+sub debug
+{
+    my ($str) = @_;
+
+    if ($DEBUG == 1)
+    {
+        print $str;
+    }
+}
